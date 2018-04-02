6e9c61ec42db53ef7b489f8f5a51b59791fab398
Dzmitry Dubarau <ddzmitry@yahoo.com>
2018-04-02 00:13:37 -0400

    More Changes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public void Intro(string to)
+        {
+            Console.WriteLine("Hello {0}, I am {1} and I love you!", to, name);
+        }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Person dzmitry = new Person("Dzmitry",25);
+            dzmitry.Intro("Anastasia");
         }
     }
 }
