c88ceb593135f76a372bda7df6aa2ec64f047e7c
Dzmitry Dubarau <ddzmitry@yahoo.com>
2018-04-11 23:01:01 -0400

    Method Override Demo

---
+++ MethodOverride/MethodOverride/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverride
+{
+
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //creating list
+            var shapes = new List<Shape>();
+           //Adding values
+           shapes.Add(new Circle{Width = 500} );
+           shapes.Add(new Triangle{Width = 500} );
+
+           shapes.Add(new Rectangle{Width = 400,Heigth = 500});
+           // init canvas
+           var canvas = new Canvas();
+           //drawing canvas
+           canvas.DrawShapes(shapes);
+        }
+    }
+}
---
+++ MethodOverride/MethodOverride/Shape.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverride
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            // Use parent method to override its own
+            Console.WriteLine("Draw Circle");
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            // Use parent method to override its own
+            Console.WriteLine("Draw Rectangle");
+        }
+    }
+    public class Triangle: Shape
+    {
+        public override void Draw()
+        {
+            // Use parent method to override its own
+            Console.WriteLine("Draw Triangle");
+        }
+    }
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Heigth { get; set; }
+        public Position Position { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("Shape Method draw");
+        }
+
+    }
+}
