7c10ed21c0ec7c4c9f4c5f68cffe33bf23abbba8
Dzmitry Dubarau <ddzmitry@yahoo.com>
2018-04-14 20:27:27 -0400

    NullableTypes

---
+++ NullableTypes/NullableTypes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? date = null;
+            Console.WriteLine("GetValueOrDefault() " + date.GetValueOrDefault());
+            Console.WriteLine("HasValue " + date.HasValue);
+            Console.WriteLine($"Value {(date.HasValue && date.HasValue)}");
+
+            // if Null Convention is to GetValueOrDefault()
+            DateTime date2 = date.GetValueOrDefault();
+            Console.WriteLine(date2);
+
+            DateTime? date3 = null;
+            DateTime date4;
+
+
+            //date4 = date3 != null ? date3.GetValueOrDefault() : DateTime.Today;
+            // same as Previous
+            date4 = date3 ?? DateTime.Today;
+            Console.WriteLine(date4);
+
+        }
+    }
+}
