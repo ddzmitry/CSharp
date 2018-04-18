4a3b25259a13d11ca664489deeb3188041f64b55
Dzmitry Dubarau <ddzmitry@yahoo.com>
2018-04-18 12:39:48 -0400

    Math ThestCases with NUnit

---
+++ UnitTesting/TestNinja/UnitTestProject1/MathTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Math = TestNinja.Fundamentals.Math;
+
+
+namespace UnitTestProject1
+{
+    [TestFixture()]
+    public class MathTest
+    {
+        private Math _math;
+        //SetUp
+        //TearDown
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+        [Test]  
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        { 
+            var result = _math.Add(1, 2);
+            Assert.That(result,Is.EqualTo(3));
+            
+        }
+
+        [Test]
+        [TestCase(2,1,2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnsGreaterArgument(int a, int b , int expectedResult)
+        {
+            var result = _math.Max(a,b);
+            Assert.That(result,Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+//        [Ignore("Because I am tupid!")]
+        public void GetOddNumbers_WhenCalled_returnsListOfOddNumbers()
+        {
+            
+            var result = _math.GetOddNumbers(3);
+            Assert.That(result,Is.EqualTo(new List<int>() { 1, 3 }));
+
+
+        }
+        [Test]
+        public void GetOddNumbers_WhenCalledWithZero_returnsEmptyList()
+        {
+            
+            var result = _math.GetOddNumbers(0);
+            Assert.That(result, Is.EqualTo(new List<int>()));
+        }
+
+    }
+}
---
+++ UnitTesting/TestNinja/UnitTestProject1/ReservationTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTest
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+            //Act
+            var result = reservation.CanBeCancelledBy(new User(){ IsAdmin = true});
+            //Assert
+            Assert.That(result,Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_UserIsUser_ReturnsTrue()
+        {
+            // Create a user
+            var user = new User();
+            //Set up that reservation is being made by this user
+            var reservation = new Reservation(){MadeBy = user};
+            // make sure we can cancel the reservation
+            var result = reservation.CanBeCancelledBy(user);
+            Assert.IsTrue(result);
+
+
+        }
+        [Test]
+        public void CanNotBeCancelledBy_UserIsNotUser_ReturnsFalse()
+        {
+            var user = new User();
+            var user2 = new User();
+            var reservation = new Reservation() { MadeBy = user };
+            //Make sure that user2 can't cancel user reservation
+            var result = reservation.CanBeCancelledBy(user2);
+            Assert.IsFalse(result);
+
+
+        }
+
+    }
+}
